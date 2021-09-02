1cef3f045f9c05afd8994366e87eb931add21d3d
Pritish Mair <59889857+Pmair20@users.noreply.github.com>
2021-09-02 21:08:17 +0530

    Update auto_move.py

--- auto_move.py
+++ auto_move.py
@@ -1,3 +1,5 @@
 #this is the start of the auto drone movement
 #Requirements of the code
-#1-
+#1- Current Position (Coordinates)
+#2- Destination Position (Coordinates)
+#3- Previous Orientation (Degree)
